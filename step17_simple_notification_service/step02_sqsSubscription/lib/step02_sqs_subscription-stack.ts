a68a71e957a117b8e607a3be6be4eb20959f001b
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2021-12-29 04:57:54 +0500

    sqs sub to topic

--- step17_simple_notification_service/step02_sqsSubscription/lib/step02_sqs_subscription-stack.ts
+++ step17_simple_notification_service/step02_sqsSubscription/lib/step02_sqs_subscription-stack.ts
@@ -1,15 +1,39 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as sns from '@aws-cdk/aws-sns';
+import * as sqs from '@aws-cdk/aws-sqs';
+import * as subscriptions from '@aws-cdk/aws-sns-subscriptions';
 
 export class Step02SqsSubscriptionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create an SNS topic
+    const myTopic = new sns.Topic(this, 'MyTopic');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Step02SqsSubscriptionQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // create a queue for subscription
+    const myQueue = new sqs.Queue(this, 'MyQueue');
+
+    // create a dead letter queue
+    const dlQueue = new sqs.Queue(this, 'DeadLetterQueue', {
+      queueName: 'MySubscription_DLQ',
+      retentionPeriod: cdk.Duration.days(1),
+    });
+
+    // subscribe queue to the topic
+    // we have also defined a filter policy here.
+    // The queue will only recieve events from SNS if the the filter policy is
+    // satisfied
+    // we have also assigned a dead letter queue to store the failed events
+
+    myTopic.addSubscription(
+      new subscriptions.SqsSubscription(myQueue, {
+        filterPolicy: {
+          test: sns.SubscriptionFilter.stringFilter({
+            whitelist: ['test'],
+          }),
+        },
+        deadLetterQueue: dlQueue,
+      })
+    );
   }
 }
