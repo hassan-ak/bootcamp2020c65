9f0bdb9be7b111f173bc90708eab09ba873db531
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2021-12-29 05:15:30 +0500

    email with topic

---
+++ step17_simple_notification_service/step03_emailSubscription/test/step03_email_subscription.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Step03EmailSubscription from '../lib/step03_email_subscription-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step03_email_subscription-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step03EmailSubscription.Step03EmailSubscriptionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
