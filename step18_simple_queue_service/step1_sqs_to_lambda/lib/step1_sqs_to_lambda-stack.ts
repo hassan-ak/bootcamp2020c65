09f803d03c4aa50ed13462accd60642a5d4012ac
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2021-12-29 07:07:26 +0500

    queue with lambda

--- step18_simple_queue_service/step1_sqs_to_lambda/lib/step1_sqs_to_lambda-stack.ts
+++ step18_simple_queue_service/step1_sqs_to_lambda/lib/step1_sqs_to_lambda-stack.ts
@@ -1,15 +1,34 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as path from 'path';
+import * as sqs from '@aws-cdk/aws-sqs';
+import * as lambdaEvents from '@aws-cdk/aws-lambda-event-sources';
 
 export class Step1SqsToLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Lambda function to get notifications
+    const sqsLambda = new lambda.Function(this, 'sqsLambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, '/../', 'lambda')),
+      handler: 'index.handler',
+      reservedConcurrentExecutions: 5, // only have 5 invocations at a time, having this <5 has a problem?
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Step1SqsToLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Defien a queue
+    const queue = new sqs.Queue(this, 'testQueue', {
+      queueName: 'testQueue',
+      encryption: sqs.QueueEncryption.UNENCRYPTED,
+      retentionPeriod: cdk.Duration.days(4),
+      visibilityTimeout: cdk.Duration.seconds(30), // default,
+      receiveMessageWaitTime: cdk.Duration.seconds(20), // default
+    });
+
+    sqsLambda.addEventSource(
+      new lambdaEvents.SqsEventSource(queue, {
+        batchSize: 10,
+      })
+    );
   }
 }
