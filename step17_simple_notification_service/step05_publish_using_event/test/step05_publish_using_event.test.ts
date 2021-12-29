3eb1276148aa9a826041183b8fc62d13ca644454
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2021-12-29 05:40:50 +0500

    cdk app

---
+++ step17_simple_notification_service/step05_publish_using_event/test/step05_publish_using_event.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Step05PublishUsingEvent from '../lib/step05_publish_using_event-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step05_publish_using_event-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step05PublishUsingEvent.Step05PublishUsingEventStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
